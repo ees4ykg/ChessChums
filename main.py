2a33bf7afdea0a71e3237cf698dc3c2e16ec3621
Toha Miazhi_ <123932308+Miazhi24@users.noreply.github.com>
2023-12-14 13:37:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#make a board
-#make functions for each piece
-#object oriented program
-#detect if a piece can be taken
-#other micelanous rules such as queening and castling and en passent
+#make a board: 8 x 8. Key name for all the squares. 
+
+{0,1,0,1,0,1,0,1}
+#make functions for each piece: 
+#object oriented program: 
+#detect if a piece can be taken: 
+#other micelanous rules such as queening and castling and en passent: 
 
 
