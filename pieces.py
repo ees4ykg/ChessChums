fdf11d7f9f8ff96670dd623d592cec1477fbb12c
ees4ykg <ghorieesa@gmail.com>
2023-12-22 11:06:38 +0000

    Completed classes for Bishop, Queen, Knight and King
    - Created general vision_update_loop function to create vision_update method for all pieces

--- pieces.py
+++ pieces.py
@@ -4,45 +4,67 @@ from turtle import Turtle
 parts = ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn']
 
 
-def set_square_piece(board, colour, co_ords, piece):
+def set_square_piece(board, colour, co_ords, name):
     if not board.reversed:  # if it is a white pawn
-        board.squares[board.squares_reversed[str(co_ords)]]['piece'] = f'{colour}_{piece}'
+        board.squares[board.squares_reversed[str(co_ords)]]['piece'] = f'{colour}_{name}'
     elif board.reversed:  # if it is a black pawn it adds the pawn to the square
-        board.squares[board.squares_reversed[str(co_ords)]]['piece'] = f'{colour}_{piece}'
+        board.squares[board.squares_reversed[str(co_ords)]]['piece'] = f'{colour}_{name}'
+
+
+def initialise_piece(self, colour, co_ords, name):
+    self.penup()
+    self.colour = colour
+    if self.colour == 'w':
+        self.opposite_colour = 'b'
+    elif self.colour == 'b':
+        self.opposite_colour = 'w'
+    self.shape(f"images/{colour}_{name}_1x_ns.gif")
+    self.setpos(co_ords)
+
+
+def vision_update_loop(self, board, directions, infinity): # Used for vision_update in rook, bishop and queen class
+    self.vision = []
+    self.capture_vision = []
+    loop = True
+
+    for dx, dy in directions:
+        xpos = self.xcor()
+        ypos = self.ycor()
+
+        while loop:
+            xpos += 75 * dx
+            ypos += 75 * dy
+
+            if str((xpos, ypos)) not in board.squares_reversed.keys():
+                break
+
+            square_piece = board.squares[board.squares_reversed[str((xpos, ypos))]]['piece']
+
+            if square_piece == 'empty':
+                self.vision.append((xpos, ypos))
+
+            elif square_piece in [f'{self.opposite_colour}_{part}' for part in parts]:
+                self.capture_vision.append((xpos, ypos))
+                break
+
+            else:
+                break
+
+            if not infinity:
+                break
 
 
 class Pawn(Turtle):
-    def __init__(self, colour, opposite_colour, co_ords, board):
-        super().__init__()
+    def __init__(self, colour, co_ords, board):
 
-        self.penup()
-        self.colour = colour
-        self.opposite_colour = opposite_colour
-        self.shape(f"images/{colour}_pawn_1x_ns.gif")
-        self.setpos(co_ords)
-        self.vision = [(self.xcor(), self.ycor() + 75), (self.xcor(), self.ycor() + 150)]
+        super().__init__()
+        initialise_piece(self, colour, co_ords, 'pawn')
+
+        self.vision = []
         self.capture_vision = []
         self.has_moved = False
 
         set_square_piece(board, colour, co_ords, "pawn")
-
-    # def movement(self):
-    #     new_x = self.pos()[0]
-    #     new_y = self.pos()[1] + 75
-    #     self.setpos(new_x, new_y)
-    #
-    # def capture(self, direction):
-    #     # check if pieces are diagonal
-    #     # if they are, delete that piece and move to its location
-    #     if direction == "left":
-    #         new_x = self.xcor() - 75
-    #         new_y = self.ycor() + 75
-    #         self.setpos(new_x, new_y)
-    #
-    #     elif direction == "right":
-    #         new_x = self.xcor() + 75
-    #         new_y = self.ycor() + 75
-    #         self.setpos(new_x, new_y)
 
     def vision_update(self, board):
 
@@ -74,125 +96,76 @@ class Pawn(Turtle):
 
 
 class Rook(Turtle):
-    def __init__(self, colour, opposite_colour, co_ords, board):
+    def __init__(self, colour, co_ords, board):
         super().__init__()
+        initialise_piece(self, colour, co_ords, 'rook')
 
-        self.penup()
-        self.colour = colour
-        self.opposite_colour = opposite_colour
-        self.shape(f"images/{colour}_rook_1x_ns.gif")
-        self.setpos(co_ords)
         self.vision = []
         self.capture_vision = []
         self.has_moved = False
 
         set_square_piece(board, colour, co_ords, "rook")
 
-    # def vision_update(self, board):
-    #     self.vision = []
-    #
-    #     for square in board.squares:
-    #         # for every square which letter is the same as the letter that the rook is on
-    #         if board.squares[square]['co_ordinates'] == self.pos():
-    #             pass
-    #
-    #         elif square[0] == board.squares_reversed[str((int(self.xcor()), int(self.ycor())))][0]:
-    #             if board.squares[square]["piece"] != "empty":
-    #                 break
-    #             self.vision.append(board.squares[square]["co_ordinates"])
-    #
-    #         # for every square which letter is the same as the number that the rook is on
-    #         elif square[1] == board.squares_reversed[str((int(self.xcor()), int(self.ycor())))][1]:
-    #             if board.squares[square]["piece"] != "empty":
-    #                 break
-    #             self.vision.append(board.squares[square]["co_ordinates"])
-    #
-    #     self.capture_vision = []
-
     def vision_update(self, board):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        vision_update_loop(self, board, directions, True)
+
+
+class Bishop(Turtle):
+    def __init__(self, colour, co_ords, board):
+        super().__init__()
+        initialise_piece(self, colour, co_ords, 'bishop')
 
         self.vision = []
         self.capture_vision = []
 
-        xpos = self.xcor()
-        ypos = self.ycor()
-        while True:
-            xpos += 75
+        set_square_piece(board, colour, co_ords, 'bishop')
 
-            if str((xpos, ypos)) not in board.squares_reversed.keys():
-                break
+    def vision_update(self, board):
+        directions = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+        vision_update_loop(self, board, directions, True)
 
-            square_piece = board.squares[board.squares_reversed[str((xpos, ypos))]]['piece']
 
-            if square_piece == 'empty':
-                self.vision.append((xpos, ypos))
+class Queen(Turtle):
+    def __init__(self, colour, co_ords, board):
+        super().__init__()
+        initialise_piece(self, colour, co_ords, 'queen')
 
-            elif square_piece in [f'{self.opposite_colour}_{part}' for part in parts]:
-                self.capture_vision.append((xpos, ypos))
-                break
+        self.vision = []
+        self.capture_vision = []
 
-            else:
-                break
+        set_square_piece(board, colour, co_ords, 'queen')
 
-        xpos = self.xcor()
-        ypos = self.ycor()
+    def vision_update(self, board):
+        directions = [(1, 1), (-1, -1), (1, -1), (-1, 1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+        vision_update_loop(self, board, directions, True)
 
-        while True:
-            xpos -= 75
 
-            if str((xpos, ypos)) not in board.squares_reversed.keys():
-                break
+class Knight(Turtle):
+    def __init__(self, colour, co_ords, board):
+        super().__init__()
+        initialise_piece(self, colour, co_ords, 'knight')
 
-            square_piece = board.squares[board.squares_reversed[str((xpos, ypos))]]['piece']
+        self.vision = []
+        self.capture_vision = []
 
-            if square_piece == 'empty':
-                self.vision.append((xpos, ypos))
+        set_square_piece(board, colour, co_ords, 'knight')
 
-            elif square_piece in [f'{self.opposite_colour}_{part}' for part in parts]:
-                self.capture_vision.append((xpos, ypos))
-                break
+    def vision_update(self, board):
+        directions = [(1, 2), (-1, 2), (1, -2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+        vision_update_loop(self, board, directions, False)
 
-            else:
-                break
 
-        xpos = self.xcor()
-        ypos = self.ycor()
+class King(Turtle):
+    def __init__(self, colour, co_ords, board):
+        super().__init__()
+        initialise_piece(self, colour, co_ords, 'king')
 
-        while True:
-            ypos += 75
+        self.vision = []
+        self.capture_vision = []
 
-            if str((xpos, ypos)) not in board.squares_reversed.keys():
-                break
+        set_square_piece(board, colour, co_ords, 'king')
 
-            square_piece = board.squares[board.squares_reversed[str((xpos, ypos))]]['piece']
-
-            if square_piece == 'empty':
-                self.vision.append((xpos, ypos))
-
-            elif square_piece in [f'{self.opposite_colour}_{part}' for part in parts]:
-                self.capture_vision.append((xpos, ypos))
-                break
-
-            else:
-                break
-
-        xpos = self.xcor()
-        ypos = self.ycor()
-
-        while True:
-            ypos -= 75
-
-            if str((xpos, ypos)) not in board.squares_reversed.keys():
-                break
-
-            square_piece = board.squares[board.squares_reversed[str((xpos, ypos))]]['piece']
-
-            if square_piece == 'empty':
-                self.vision.append((xpos, ypos))
-
-            elif square_piece in [f'{self.opposite_colour}_{part}' for part in parts]:
-                self.capture_vision.append((xpos, ypos))
-                break
-
-            else:
-                break
+    def vision_update(self, board):
+        directions = [(1, 1), (-1, -1), (1, -1), (-1, 1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+        vision_update_loop(self, board, directions, False)
